334220472f68cdd68f6d5524932497b0b8e6fb85
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-04-03 16:43:16 +0200

    fixed 2 tasks

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,36 +1,37 @@
-const form = document.querySelector('.feedback-form');
-const emailInput = form.querySelector('input[name="email"]');
-const messageInput = form.querySelector('textarea[name="message"]');
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.feedback-form');
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
 
-form.addEventListener('input', () => {
-  const formData = {
-    email: emailInput.value.trim(),
-    message: messageInput.value.trim()
-  };
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-});
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-  if (formData.email && formData.message) {
-    console.log(formData);
-    localStorage.removeItem('feedback-form-state');
-    emailInput.value = '';
-    messageInput.value = ''; 
-  } else {
-    console.log('Please fill in both email and message fields.');
-  }
-});
-
-window.addEventListener('beforeprint', () => {
-  const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  const formData =
+    JSON.parse(localStorage.getItem('feedback-form-state')) || {};
   if (formData.email) {
     emailInput.value = formData.email;
   }
   if (formData.message) {
     messageInput.value = formData.message;
   }
+
+  form.addEventListener('input', () => {
+    const formData = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  });
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    const formData =
+      JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+    if (formData.email && formData.message) {
+      console.log(formData);
+      localStorage.removeItem('feedback-form-state');
+      emailInput.value = '';
+      messageInput.value = '';
+      alert('Дякуємо за ваш відгук!');
+    } else {
+      alert('Будь ласка, заповніть поля електронної пошти та повідомлення.');
+    }
+  });
 });
-
-
